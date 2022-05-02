9efafb22e68b1c7f576e56cc8087ccf5f79798e5
nivedano <nivedano@hyperonym.net>
2022-05-02 16:21:46 +0300

    schemas url changed

--- schema_validation/validate.py
+++ schema_validation/validate.py
@@ -6,7 +6,7 @@ from typing import Any, Callable
 from unittest import result
 import jsonschema.validators as jsv
 
-BASE_PATH = os.path.abspath(os.getcwd())
+BASE_PATH = os.path.abspath(os.getcwd()) + '/schemas'
 
 class JSONWithCommentsDecoder(json.JSONDecoder):
     def __init__(self, **kw):
@@ -27,6 +27,7 @@ def jload(fn: str):
 
 predicate_schema = jload('schemas/meta/predicate')
 vccs_schema = jload('schemas/meta/vccs')
+vc_schema = jload('schemas/vc/vc_jwt_v1')
 
 cache = {}
 
@@ -34,8 +35,8 @@ def local_ref_resolve_remote(inherited, uri: str):
     result = cache.get(uri)
     if result is not None:
         return result
-    if uri.startswith('https://identix.space'):
-        result = jload(uri.replace('https://identix.space', BASE_PATH))
+    if uri.startswith('https://schemas.identix.space'):
+        result = jload(uri.replace('https://schemas.identix.space', BASE_PATH))
         cache[uri] = result
         return result
     return inherited(uri)
@@ -43,24 +44,28 @@ def local_ref_resolve_remote(inherited, uri: str):
 idx_pred_validator = jsv.Draft202012Validator(predicate_schema)
 inherited = idx_pred_validator.resolver.resolve_remote
 idx_pred_validator.resolver.resolve_remote = lambda url: local_ref_resolve_remote(inherited, url)
+
 idx_vccs_validator = jsv.Draft202012Validator(vccs_schema)
 inherited = idx_vccs_validator.resolver.resolve_remote
 idx_vccs_validator.resolver.resolve_remote = lambda url: local_ref_resolve_remote(inherited, url)
 
-predicates = list(map(jload, glob(BASE_PATH + '/schemas/core/*')))
-predicates += list(map(jload, glob(BASE_PATH + '/schemas/officials/*')))
-examples = list(map(jload, glob(BASE_PATH + '/schemas/examples/*')))
+idx_vc_validator = jsv.Draft202012Validator(vc_schema)
+inherited = idx_vc_validator.resolver.resolve_remote
+idx_vc_validator.resolver.resolve_remote = lambda url: local_ref_resolve_remote(inherited, url)
+
+predicates = list(map(jload, glob(BASE_PATH + '/core/*'))) \
+           + list(map(jload, glob(BASE_PATH + '/officials/*')))
+vccs_examples = list(map(jload, glob(BASE_PATH + '/examples/vccs/*')))
+vc_examples = list(map(jload, glob(BASE_PATH + '/examples/vc/*')))
 
 for p in predicates:
     idx_pred_validator.validate(p)
     print(p['id'])
 
-for vcs in examples:
+for vcs in vccs_examples:
     idx_vccs_validator.validate(vcs)
-    schema: str = vcs["$schema"]
-    if schema.endswith('/vccs'):
-        print(vcs['id'])
-    elif schema.endswith('/vc_jwt_v1'):
-        print(vcs['payload']['jti'])
-    else:
-        print('What the hell are you?')
+    print(vcs['id'])
+
+for vcs in vc_examples:
+    idx_vc_validator.validate(vcs)
+    print(vcs['payload']['jti'])
